d5c03b1142c0a1c7a6e42e427712f94c8cde6d5c
Liu WenXing <liuwenxing@yanxiu.com>
2017-09-26 17:30:28 +0800

    Fix

--- FaceShowApp/UI/首页/CourseDetailViewController.m
+++ FaceShowApp/UI/首页/CourseDetailViewController.m
@@ -67,6 +67,12 @@
         [self.dataArray addObject:!isEmpty(self.data.interactSteps) ? self.data.interactSteps : [NSArray array]];
         self.tableView.hidden = NO;
         self.headerView.course = self.data.course;
+        NSLayoutConstraint *widthFenceConstraint = [NSLayoutConstraint constraintWithItem:self.headerView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:SCREEN_WIDTH];
+        [self.headerView addConstraint:widthFenceConstraint];
+        [self.headerView removeConstraint:widthFenceConstraint];
+        CGSize fittingSize = [self.headerView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+        self.headerView.frame = CGRectMake(0, 0, SCREEN_WIDTH, fittingSize.height);
+        self.tableView.tableHeaderView = self.headerView;
         [self.tableView layoutIfNeeded];
         [self.tableView reloadData];
     }];
@@ -97,11 +103,7 @@
         courseBriefVC.courseBrief = self.data.course.briefing;
         [self.navigationController pushViewController:courseBriefVC animated:NO];
     };
-    self.tableView.tableHeaderView = self.headerView;
-    [self.headerView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(0);
-        make.width.mas_equalTo(SCREEN_WIDTH);
-    }];
+    
     [self.tableView registerClass:[CourseCatalogCell class] forCellReuseIdentifier:@"CourseCatalogCell"];
     [self.view addSubview:self.tableView];
     [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
