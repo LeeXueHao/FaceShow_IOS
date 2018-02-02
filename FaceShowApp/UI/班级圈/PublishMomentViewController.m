0e55b8dda5b5ceb908bb37caa0c0e4b9fdf552bd
niuzhaowang <niuzhaowang.yanxiu.com>
2018-02-02 12:00:20 +0800

    commit

--- FaceShowApp/UI/班级圈/PublishMomentViewController.m
+++ FaceShowApp/UI/班级圈/PublishMomentViewController.m
@@ -325,13 +325,13 @@
 //        return NO;
 //    }
     NSString *str = [textView.text stringByReplacingCharactersInRange:range withString:text];
-    if (str.length > 20) {
-        str = [str substringToIndex:20];
+    if (str.length > 200) {
+        str = [str substringToIndex:200];
         textView.text = str;
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             UITextPosition* beginning = textView.beginningOfDocument;
-            UITextPosition* startPosition = [textView positionFromPosition:beginning offset:20];
-            UITextPosition* endPosition = [textView positionFromPosition:beginning offset:20];
+            UITextPosition* startPosition = [textView positionFromPosition:beginning offset:200];
+            UITextPosition* endPosition = [textView positionFromPosition:beginning offset:200];
             UITextRange* selectionRange = [textView textRangeFromPosition:startPosition toPosition:endPosition];
             [textView setSelectedTextRange:selectionRange];
         });
