60ad2ff8615e999550ca243e7570cc8dbe027a36
zhoulili <zhoulili@yanxiu.com>
2018-04-04 14:29:23 +0800

    fix

--- FaceShowApp/UI/IM/ChatViewController.m
+++ FaceShowApp/UI/IM/ChatViewController.m
@@ -150,6 +150,7 @@ NSString * const kIMUnreadMessageCountClearNotification = @"kIMUnreadMessageCoun
                 }
             }
         }];
+        [self scrollToBottom];
         [TalkingData trackEvent:@"点击聊聊相机"];
     }];
     [self.view addSubview:self.imInputView];
