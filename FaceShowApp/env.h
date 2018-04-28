050bf9056ed0d2512f85b1bdb48ca556a20250dd
niuzhaowang <niuzhaowang.yanxiu.com>
2018-04-28 14:24:42 +0800

    commit

---
+++ FaceShowApp/env.h
@@ -0,0 +1,14 @@
+//
+//  env.h
+//  FaceShowApp
+//
+//  Created by niuzhaowang on 2018/4/28.
+//  Copyright © 2018年 niuzhaowang. All rights reserved.
+//
+
+#ifndef env_h
+#define env_h
+
+#undef DEBUG
+
+#endif /* env_h */
