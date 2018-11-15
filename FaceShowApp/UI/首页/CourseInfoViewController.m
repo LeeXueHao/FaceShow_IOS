af5e1b68503ce9fc2524677cc773dbfaa15990a1
LeeXueHao <lixuehao@yanxiu.com>
2018-11-15 17:05:50 +0800

    fix

--- FaceShowApp/UI/首页/CourseInfoViewController.m
+++ FaceShowApp/UI/首页/CourseInfoViewController.m
@@ -29,17 +29,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSArray<NSString *> *tabArr;
     switch (self.type) {
         case CourseInfoType_Default:
+        {
             self.navigationItem.title = @"课程信息";
+            tabArr = @[@"讲师",@"简介"];
+        }
             break;
         case CourseInfoType_NingBoMeeting:
+        {
             self.navigationItem.title = @"会议信息";
+            tabArr = @[@"专家",@"简介"];
+        }
             break;
         default:
             break;
     }
-    [self setupUI];
+    [self setupUIWithTabArr:tabArr];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,9 +54,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)setupUI {
+- (void)setupUIWithTabArr:(NSArray<NSString *> *)tabArr {
     CourseInfoTabContainerView *tabContainerView = [[CourseInfoTabContainerView alloc]init];
-    tabContainerView.tabNameArray = @[@"讲师",@"简介"];
+    tabContainerView.tabNameArray = tabArr;
     WEAK_SELF
     [tabContainerView setTabClickBlock:^(NSInteger index){
         STRONG_SELF
